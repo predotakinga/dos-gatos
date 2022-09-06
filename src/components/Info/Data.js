88c4ecd5cb214e7d1d913813220068c6659e1257
predotakinga <56541567+predotakinga@users.noreply.github.com>
2022-09-06 15:48:11 +0200

    Update Data.js

--- src/components/Info/Data.js
+++ src/components/Info/Data.js
@@ -6,7 +6,7 @@ export const HoursObjOne = {
   topLine: "Wpadnij nas odwiedzić",
   headline: "Godziny otwarcia",
   description:
-    "Gdybyśmy tylko mogli, gotowalibyśmy dla Was całą dobę w każdy dzień tygodnia. Niestety też musimy udać się czasem na fiestę, dlatego jesteśmy dostępni dla naszych ukochanych gości w tych godzinach. Zapraszamy serdecznie na południowy lunch, popołudniowy obiad czy wieczorne jedzenie.",
+    "Gdybyśmy tylko mogli, gotowalibyśmy dla Was całą dobę w każdy dzień tygodnia. Niestety też musimy udać się czasem na siestę, dlatego jesteśmy dostępni dla naszych ukochanych gości w tych godzinach. Zapraszamy serdecznie na południowy lunch, popołudniowy obiad czy wieczorne jedzenie.",
   imgStart: true,
   img: require("../../images/hours.jpg"),
   alt: "Godziny",
